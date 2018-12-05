3cc29739d5b01b675e64fbfea17e5f8be1fdc842
Max Lobur <max_lobur@outlook.com>
2018-12-05 13:47:11 +0200

    hack version

--- ops/env.sh
+++ ops/env.sh
@@ -1,2 +1,2 @@
-APP_VERSION=`cat Version`
-DOCKER_TAG=${DOCKER_TAG:-"quay.io/verygoodsecurity/domain-scan:v$APP_VERSION"}
+APP_VERSION=`cat helm/domain-scan/values.yaml | grep tag: | cut -d':' -f2 | awk '{$1=$1};1'`
+DOCKER_TAG=${DOCKER_TAG:-"quay.io/verygoodsecurity/domain-scan:$APP_VERSION"}
