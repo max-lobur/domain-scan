a51e52d1c9f23f6c7da036e79df110a9fc2922db
Oleksandr.Medvid <oleksandr.medvid@vgs.io>
2018-12-24 20:59:07 +0200

    Impruvments according to comments [ch15488]

--- scan.sh
+++ scan.sh
@@ -8,20 +8,23 @@ SKIP_DOMAINS=${SKIP_DOMAINS:-"example.example.com"}
 AMASS_OPTS=${AMASS_OPTS:-"-passive -include-unresolvable -noalts -T 0 -r 1.1.1.1"}  # https://github.com/OWASP/Amass#using-the-tool-suite
 AQUATONE_OPTS=${AQUATONE_OPTS:-"-debug -save-body false -scan-timeout 300 -threads 1"}  # https://github.com/michenriksen/aquatone#command-line-options
 ROLLBAR_TOKEN=${ROLLBAR_TOKEN:-""}
+REPORT_URL=${REPORT_URL:-""}
 
-function dirs() {
-    if [ ! -f aquatone_report.html ]; then
-        echo "Aqatone report not found!"
-    else
-        mkdir -p $AQUATONE_REPO/$DATE
-        if [ -f $AQUATONE_REPO/$DATE/index.html ]; then
+function start() {
+    mkdir -p $AQUATONE_REPO/$DATE
+        if [ -z "$(ls -A $AQUATONE_REPO/$DATE)" ]; then
+            echo "The folder looks like newly created"
+        else
             echo "Report already exists!"
+            echo "let's clean old files."
             rm -rf $AQUATONE_REPO/$DATE/*
         fi
-        cat aquatone_report.html > $AQUATONE_REPO/$DATE/index.html
-        mv -f headers html screenshots $AQUATONE_REPO/$DATE
-        find $AQUATONE_REPO/* -type d -mtime +10 | xargs rm -rf
-    fi
+    cd $AQUATONE_REPO/$DATE
+}
+
+function clelanup_reports() {
+    find $AQUATONE_REPO/* -type d -mtime +10 | xargs rm -rf
+    rm -rf *.txt *.log rollbar.json
 }
 
 function scan() {
@@ -31,30 +34,41 @@ function scan() {
     echo "\n\nStarting aquatone scan of discovered domains.\n" | tee -a scan.log
     touch aquatone_urls.txt
     cat hosts.txt | uniq | aquatone ${AQUATONE_OPTS} | tee -a scan.log
-    dirs
 }
 
 function report() {
     urls=`cat aquatone_urls.txt | wc -l`
     if [[ ${urls} -ge 0 ]]; then
-        echo "Discovered URLs:" | tee -a scan.log
-        cat aquatone_urls.txt | tee -a scan.log
+        if [ ! -f aquatone_report.html ]; then
+            echo "Discovered URLs:" | tee -a scan.log
+            cat aquatone_urls.txt | tee -a scan.log
+        else
+            mv aquatone_report.html index.html
+            echo "All recent reports here: https://$REPORT_URL \n" > report.log
+        fi
         report_rollbar
-        rm -rf *.*
+        clelanup_reports
     else
         echo "Found no URLs"
+        rm -rf $AQUATONE_REPO/$DATE
     fi
 }
 
 function report_rollbar() {
     if [[ ! -z "$ROLLBAR_TOKEN" ]]; then
         echo "Sending rollbar notification"
-        title="Domain Scan has found $urls public (potentially vulnerable) URLs"
-        body="==> Discovered URLs:\n"`cat aquatone_urls.txt`"\n\n==> Scan log:\n"`cat scan.log`
+        if [ ! -f report.log ]; then
+            title="Domain Scan has found $urls public (potentially vulnerable) URLs"
+            body="==> Discovered URLs:\n"`cat aquatone_urls.txt`"\n\n==> Scan log:\n"`cat scan.log`
+        else            
+            title="Domain Scan has found $urls public (potentially vulnerable) URLs, takeover report: https://$REPORT_URL/$DATE"
+            body="==> Discovered URLs:\n"`cat aquatone_urls.txt`"\n\n==> Report URL:\n"`cat report.log`
+        fi
         echo "{\"data\": {\"environment\": \"external\", \"level\": \"warn\", \"title\": \"$title\", \"body\": {\"message\": {\"body\": \"$body\"}}}, \"access_token\": \"$ROLLBAR_TOKEN\"}" > rollbar.json
         curl -H "Content-Type: application/json" --data-binary "@rollbar.json" https://api.rollbar.com/api/1/item/
     fi
 }
 
+start
 scan
 report
