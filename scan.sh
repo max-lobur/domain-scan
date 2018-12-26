b453556b7f48ce65c5ab56841af6e6d1c514ac53
Oleksandr.Medvid <oleksandr.medvid@vgs.io>
2018-12-26 22:40:32 +0200

    more fixes [ch15488]

--- scan.sh
+++ scan.sh
@@ -10,44 +10,38 @@ AQUATONE_OPTS=${AQUATONE_OPTS:-"-debug -save-body false -scan-timeout 300 -threa
 ROLLBAR_TOKEN=${ROLLBAR_TOKEN:-""}
 REPORT_URL=${REPORT_URL:-""}
 
-function start() {
-    mkdir -p $AQUATONE_REPO/$DATE
-        if [ -z "$(ls -A $AQUATONE_REPO/$DATE)" ]; then
-            echo "The folder looks like newly created"
-        else
-            echo "Report already exists!"
-            echo "let's clean old files."
-            rm -rf $AQUATONE_REPO/$DATE/*
-        fi
+function mk_scan_dir() {
+    rm -rf $AQUATONE_REPO/$DATE
+    mkdir -p $AQUATONE_REPO/$DATE 
     cd $AQUATONE_REPO/$DATE
 }
 
-function clelanup_reports() {
+function cleanup_scans() {
     find $AQUATONE_REPO/* -type d -mtime +10 | xargs rm -rf
     rm -rf rollbar.json
+    chmod -R go=rX,u=rwX ./*
 }
 
 function scan() {
-    echo "Starting amass scan of '$SCAN_DOMAINS'.\n" | tee -a scan.txt
-    amass ${AMASS_OPTS} -d ${SCAN_DOMAINS} -o hosts.txt | tee -a scan.txt
-    filter_domains.py | tee -a scan.txt  # amass -bl does not work so have to filter.
-    echo "\n\nStarting aquatone scan of discovered domains.\n" | tee -a scan.txt
+    echo "Starting amass scan of '$SCAN_DOMAINS'.\n" | tee -a scan.log.txt
+    amass ${AMASS_OPTS} -d ${SCAN_DOMAINS} -o hosts.txt | tee -a scan.log.txt
+    filter_domains.py | tee -a scan.log.txt  # amass -bl does not work so have to filter.
+    echo "\n\nStarting aquatone scan of discovered domains.\n" | tee -a scan.log.txt
     touch aquatone_urls.txt
-    cat hosts.txt | uniq | aquatone ${AQUATONE_OPTS} | tee -a scan.txt
+    cat hosts.txt | uniq | aquatone ${AQUATONE_OPTS} | tee -a scan.log.txt
 }
 
 function report() {
     urls=`cat aquatone_urls.txt | wc -l`
     if [[ ${urls} -ge 0 ]]; then
         if [ ! -f aquatone_report.html ]; then
-            echo "Discovered URLs:" | tee -a scan.txt
-            cat aquatone_urls.txt | tee -a scan.txt
+            echo "Discovered URLs:" | tee -a scan.log.txt
+            cat aquatone_urls.txt | tee -a scan.log.txt
         else
             mv aquatone_report.html report.html
             echo "All recent reports here: https://$REPORT_URL \n" > report.txt
         fi
         report_rollbar
-        clelanup_reports
     else
         echo "Found no URLs"
         rm -rf $AQUATONE_REPO/$DATE
@@ -57,28 +51,23 @@ function report() {
 function report_rollbar() {
     if [[ ! -z "$ROLLBAR_TOKEN" ]]; then
         echo "Sending rollbar notification"
-        if [ ! -f report.txt ]; then
-            mess=" "
-            file="scan.txt"
-            warn_level="warn"
+        warn_level="warning"
+        report_page=https://$REPORT_URL/$DATE
+        file="report.txt"
+        if grep -i "takeover" report.html 
+        then
+            title="Domain Scan has found takeover threat! Report: $report_page/report.html"
+            warn_level="error"
         else
-            if grep -i "Domain Takeover" report.html 
-            then
-                mess=", found possible domain takeover, report here: https://$REPORT_URL/$DATE/report.html"
-                warn_level="critical"
-            else
-                mess=", report here: https://$REPORT_URL/$DATE"
-                warn_level="warn"
-            fi            
-            file="report.txt"
-        fi
-        title="Domain Scan has found $urls public (potentially vulnerable) URLs$mess"
+            title="Domain Scan has found $urls public (potentially vulnerable) URLs. Report: $report_page"
+        fi            
         body="==> Discovered URLs:\n"`cat aquatone_urls.txt`"\n\n==> Scan log:\n"`cat $file`
         echo "{\"data\": {\"environment\": \"external\", \"level\": \"$warn_level\", \"title\": \"$title\", \"body\": {\"message\": {\"body\": \"$body\"}}}, \"access_token\": \"$ROLLBAR_TOKEN\"}" > rollbar.json
         curl -H "Content-Type: application/json" --data-binary "@rollbar.json" https://api.rollbar.com/api/1/item/
     fi
 }
 
-start
+mk_scan_dir
 scan
 report
+cleanup_scans
