36bf3671ba4ccff6ca17984aa3c620b94de20723
Oleksandr.Medvid <oleksandr.medvid@vgs.io>
2019-02-05 14:20:55 +0200

    Healthcheck fix according to [ch20316]

--- scan.sh
+++ scan.sh
@@ -8,11 +8,12 @@ SKIP_DOMAINS=${SKIP_DOMAINS:-"example.example.com"}
 AMASS_OPTS=${AMASS_OPTS:-"-passive -include-unresolvable -noalts -T 0 -r 1.1.1.1"}  # https://github.com/OWASP/Amass#using-the-tool-suite
 AQUATONE_OPTS=${AQUATONE_OPTS:-"-debug -save-body false -scan-timeout 300 -threads 1"}  # https://github.com/michenriksen/aquatone#command-line-options
 ROLLBAR_TOKEN=${ROLLBAR_TOKEN:-""}
+HEALTHCHECKS_KEY=${HEALTHCHECKS_KEY:-""}
 REPORTS_HOSTNAME=${REPORTS_HOSTNAME:-""}
 
 function mk_scan_dir() {
     rm -rf $AQUATONE_REPO/$DATE
-    mkdir -p $AQUATONE_REPO/$DATE 
+    mkdir -p $AQUATONE_REPO/$DATE
     cd $AQUATONE_REPO/$DATE
 }
 
@@ -24,7 +25,7 @@ function scan() {
     echo "Starting amass scan of '$SCAN_DOMAINS'.\n" | tee -a scan.log.txt
     amass ${AMASS_OPTS} -d ${SCAN_DOMAINS} -o hosts.txt | tee -a scan.log.txt
     filter_domains.py | tee -a scan.log.txt  # amass -bl does not work so have to filter.
-
+    
     echo "\n\nStarting aquatone scan of discovered domains.\n" | tee -a scan.log.txt
     touch aquatone_urls.txt aquatone_report.html
     cat hosts.txt | uniq | aquatone ${AQUATONE_OPTS} | tee -a scan.log.txt
@@ -42,27 +43,33 @@ function report() {
     fi
 }
 
+function healthchecks() {
+    if [ -n "$HEALTHCHECKS_KEY" ]; then
+        wget https://hc-ping.com/"$HEALTHCHECKS_KEY" -O /dev/null
+    fi
+}
+
 function report_rollbar() {
     if [[ ! -z "$ROLLBAR_TOKEN" ]]; then
         echo "Sending rollbar notification"
         this_report_page="https://$REPORTS_HOSTNAME/$DATE/aquatone_report.html"
-
+        
         level="warning"
         title="Domain Scan has found $urls public (potentially vulnerable) URLs. Report: ${this_report_page}."
         if grep -i "takeover" aquatone_report.html; then
             level="error"
             title="Domain Scan has found a domain takeover threat! Report: ${this_report_page}."
         fi
-
+        
         body="Recent reports: https://$REPORTS_HOSTNAME/"
-
+        
         echo "{\"data\": {\"environment\": \"external\", \"level\": \"$level\", \"title\": \"$title\", \"body\": {\"message\": {\"body\": \"$body\"}}}, \"access_token\": \"$ROLLBAR_TOKEN\"}" > rollbar.json
         curl -H "Content-Type: application/json" --data-binary "@rollbar.json" https://api.rollbar.com/api/1/item/
     fi
 }
 
-
 mk_scan_dir
 scan
 report
 cleanup_scans
+healthchecks
